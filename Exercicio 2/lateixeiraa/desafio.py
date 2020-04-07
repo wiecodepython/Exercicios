4fb6ba527a6e84de970d70e69ab1230d5017544e
Larissa <larissa.silva@ee.ufcg.edu.br>
2020-04-07 18:42:22 -0300

    Respostas da semana 2

---
+++ Exercicio 2/lateixeiraa/desafio.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import math
+
+#Desafios
+
+#Questão1.
+print("Questão1")
+
+a= 100-413*(20-5*4)
+b=a/5
+print(b)
+
+#Questão2.
+
+c1=10
+c2=22
+c3=6.8
+
+cp=c1+c2+c3
+
+cs=1/(1/c1 +1/c2 +1/c3)
+print(cp)
+print(cs)
+
+#Questão 3.
+
+cebola=(420*5.4)/1000.0
+alho=(250*30)/1000.0
+pao=(450*25)/1000.0
+
+total=(75*2.20+2*8.73+3.45+cebola+alho+pao)/5
+
+print(total)
+
+#Questão 4.
+
+b=1.2
+vp=15*10*13
+vb=(3*math.pi*b**3)/4
+
+espaço=(74*vp)/100
+total=espaço//vb
+print(total,'bolinhos')
+
+
+#print(vp)
+#print(vb)
+
+
+
+
+
+
