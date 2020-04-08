e066fd83272b22f26dc1f115d755d6fb617983c6
Marina Batista <marina.batista@ee.ufcg.edu.br>
2020-04-08 20:06:38 -0300

    exercicio semana 2

---
+++ Exercicio 2/maarinaabatista/desafio_semana2.py
@@ -0,0 +1,46 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:57:15) [MSC v.1915 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #DESAFIOS
+>>> # QUESTÃO 1
+>>> (100 - 413 * (20 - 5 * 4))/5
+20.0
+>>> #QUESTÃO 2
+>>> #PARALELO
+>>> c1 = 10
+>>> c2 = 22
+>>> c3 = 6.8
+>>> cp = c1 + c2 + c3
+>>> print ("O valor resultante no primeiro caso é: ", cp , "uf")
+O valor resultante no primeiro caso é:  38.8 uf
+>>> #SÉRIE
+>>> c = 1/(1/c1 + 1/c2 + 1/c3))
+SyntaxError: invalid syntax
+>>> (1/(1c1 + 1/c2 + 1/c3))
+SyntaxError: invalid syntax
+>>> c = (1/(1/c1 + 1/c2 + 1/c3))
+>>> c = 1/c
+>>> print(" O valor resultante no segundo caso é: " , c ,"uf")
+ O valor resultante no segundo caso é:  0.2925133689839572 uf
+>>> #questão 3
+>>> cv = 75 * 2.20
+>>> mc = 2 * 8.73
+>>> tm = 3.45
+>>> cb = (420/1000) * 5.40
+>>> al = (250/1000) * 30
+>>> pao = (450/1000) * 25
+>>> total = cv + mc + tm + cb + al + pao
+>>> div = total / 4
+>>> print("O total das compras foi R$" ,total, "cada um dos amigos terá que pagar R$", div)
+O total das compras foi R$ 206.928 cada um dos amigos terá que pagar R$ 51.732
+>>> #questão 4
+>>> import math
+>>> ves = 3/
+SyntaxError: invalid syntax
+>>> ves = 3/4 * 3.14 * (1.2 ** 3)
+>>> vpote = 15 * 10 * 13
+>>> fem = 0.74
+>>> esp = vpote / fem
+>>> tot  = esp // ves
+>>> print (tot)
+647.0
+>>> 
