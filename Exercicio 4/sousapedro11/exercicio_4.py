3bbcca8b1173eb5e932ef53d40d01a8b592e5fc7
Pedro Sousa <ppls2106@gmail.com>
2020-05-14 14:26:42 -0300

    add solucoes exercicio 4

--- Exercicio 4/sousapedro11/exercicio_4.py
+++ Exercicio 4/sousapedro11/exercicio_4.py
@@ -1,9 +1,10 @@
+from random import randint
 """
 1. Crie uma lista com o nome das 3 pessoas mais próximas.
 """
 print('Exercicio 4')
 print('q1.')
-lista_pessoas = ['Pedro', 'Alexandre', 'Luiza', 'Marta', 'Miguel', 'Claudia']
+lista_pessoas = ['Pedro', 'Marta', 'Miguel']
 print(f'lista de pessoas = {lista_pessoas}')
 
 
@@ -74,7 +75,7 @@ for x in listona:
         indice = x.index(elemento)
         indice_listinha = listona.index(x)
 
-print(f'O {palavra_buscada} esta no indice {indice} do elemento de indice {indice_listinha} da listona')
+print(f'Sim. O {palavra_buscada} esta no indice {indice} do elemento de indice {indice_listinha} da listona')
 
 print('\nc.')
 listona[indice_listinha].append('brigadeiro')
@@ -89,20 +90,61 @@ listona.append('refrigerante')
 listona.append('cerveja')
 listona.append('vodka')
 listona.append('rum')
-print(listona)
+print(f'listona = {listona}')
+print('Adicionei 4 bebidas no final da lista. Coloquei refrigerante, pra nao pensarem que sou bebum...')
 
 """
 3. Utilizando o del, remova todos os elementos da lista criada anteriormente até a
 lista ficar vazia.
 """
 print('\nq3.')
+print('Removendo elementos de listona...')
+del listona[:]
+print(f'listona = {listona}')
 
 """
 4. Faça uma lista de compras do mês, não se esqueça de comprar produtos de limpeza e
 sorvete!
 Agora «vá ao mercado» e delete apenas os produtos de limpeza da lista.
 Agora «vá à sorveteria» e se empanturre de sorvete e tire o sorvete da lista.
+"""
+print('\nq4.')
+lista_compras = [
+    'arroz',
+    'feijao',
+    'macarrao',
+    'carne',
+    'um monte de miojo',
+    'farinha',
+    'ovos',
+    'detergente',
+    'sabonete',
+    'esponja',
+    'desinfetante',
+    'sorvete'
+]
 
+print(f'lista de compras = {lista_compras}')
+lista_compras.remove('detergente')
+lista_compras.remove('sabonete')
+lista_compras.remove('esponja')
+lista_compras.remove('desinfetante')
+
+print('\ndepois do mercado...')
+print(f'lista de compras = {lista_compras}')
+
+lista_compras.remove('sorvete')
+print('\ndepois da sorveteria...')
+print(f'lista de compras = {lista_compras}')
+print('\nAinda falta comprar o resto!')
+
+"""
 5. Dado uma lista de números, faça com que os números sejam ordenados e, em seguida, inverta a ordem da lista
 usando slicing.
 """
+print('\nq5.')
+lista_numeros = [randint(0, 99) for x in range(20)]
+print(f'lista de numeros aleatorios = {lista_numeros}')
+lista_numeros.sort()
+print(f'lista ordenada = {lista_numeros}')
+print(f'lista com ordem invertida = {lista_numeros[::-1]}')
