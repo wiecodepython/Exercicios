0204a12bf8b137e3b08ad45c5a7a620da6253cb6
Pedro Sousa <ppls2106@gmail.com>
2020-05-15 19:36:33 -0300

    add q1 e q2 do cap 13

--- Exercicio 5/sousapedro11/exercicio_5.py
+++ Exercicio 5/sousapedro11/exercicio_5.py
@@ -12,6 +12,7 @@ pessoas = {
 }
 
 print('EXERCICIO 5')
+print('CAP 11')
 print('q1.')
 print(f'Dicionario pessoas = {pessoas}')
 
@@ -62,3 +63,49 @@ filmes['Senhor dos Aneis: A sociedade do anel'] = {
 print('\nq3.')
 # Me recuso a colocar alguns como vilões, tipo Darth Vader ou Coringa...:)
 print(f'Dicionario filmes = {filmes}')
+
+"""
+Cap 13 - Estruturas de Controle
+
+1. Escreva um programa que, dados 2 números diferentes (a e b), encontre o menor deles.
+"""
+print('\nCAP 13')
+print('q1.')
+numero_1 = int(input('Informe o um número: '))
+numero_2 = int(input('Informe outro um número: '))
+menor = None
+if numero_1 > numero_2:
+    menor = numero_2
+elif numero_1 < numero_2:
+    menor = numero_1
+
+if menor:
+    print(f'O menor numero entre {numero_1} e {numero_2} é: {menor}')
+else:
+    print(f'Os numeros {numero_1} e {numero_2} sao iguais')
+
+"""
+2. Para doar sangue é necessário1 :
+• Ter entre 16 e 69 anos.
+• Pesar mais de 50 kg.
+• Estar descansado (ter dormido pelo menos 6 horas nas últimas 24 horas).
+Faça um programa que pergunte a idade, o peso e quanto dormiu nas últimas 24 h para uma pessoa e diga se ela
+pode doar sangue ou não.
+"""
+
+idade = int(input(''))
+
+"""
+3. Considere uma equação do segundo grau f (x) = a · x2 + b · x + c. A partir dos coeficientes, determine se a
+equação possui duas raízes reais, uma, ou se não possui.
+Dica: ∆ = b2 − 4 · a · c : se delta é maior que 0, possui duas raízes reais; se delta é 0, possui uma raiz; caso
+delta seja menor que 0, não possui raiz real
+4. Leia dois números e efetue a adição. Caso o valor somado seja maior que 20, este deverá ser apresentado
+somando-se a ele mais 8; caso o valor somado seja menor ou igual a 20, este deverá ser apresentado subtraindo-
+se 5.
+5. Leia um número e imprima a raiz quadrada do número caso ele seja positivo ou igual a zero e o quadrado do
+número caso ele seja negativo.
+6. Leia um número inteiro entre 1 e 12 e escreva o mês correspondente. Caso o usuário digite um número fora
+desse intervalo, deverá aparecer uma mensagem informando que não existe mês com este número.
+
+"""
