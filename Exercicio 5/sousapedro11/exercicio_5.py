384e6f1fc94c343b38685636f72f41e2fcea9909
Pedro Sousa <ppls2106@gmail.com>
2020-05-20 12:30:09 -0300

    add q6 do cap13

--- Exercicio 5/sousapedro11/exercicio_5.py
+++ Exercicio 5/sousapedro11/exercicio_5.py
@@ -191,6 +191,28 @@ else:
 	print(f'sqrt({num}) = {sqrt(num)}')
 
 """
-6. Leia um número inteiro entre 1 e 12 e escreva o mês correspondente. Caso o usuário digite um número fora
-desse intervalo, deverá aparecer uma mensagem informando que não existe mês com este número.
+6. Leia um número inteiro entre 1 e 12 e escreva o mês correspondente. Caso o usuário
+digite um número fora desse intervalo, deverá aparecer uma mensagem informando que
+não existe mês com este número.
 """
+print('\nq6.')
+mes = int(input('Informe um numero correspondente a um mes (ex: para junho -> 6): '))
+mes_dict = {
+	1: 'Janeiro',
+	2: 'Fevereiro',
+	3: 'Marco',
+	4: 'Abril',
+	5: 'Maio',
+	6: 'Junho',
+	7: 'Julho',
+	8: 'Agosto',
+	9: 'Setembro',
+	10: 'Outubro',
+	11: 'Novembro',
+	12: 'Dezembro',
+}
+
+while mes<1 or mes>12:
+	mes = int(input('Nao existe mes correspondente a esse numero! Informe algum no intervalo de 1 a 12: '))
+
+print(f'Mes correspondente a {mes} = {mes_dict.get(mes)}')
